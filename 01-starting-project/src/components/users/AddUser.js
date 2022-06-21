78af32b5792591aa5bf927c6e69f3a5ed8f280ea
Aman Amjad <aman.amjad@gmail.com>
2022-06-21 10:04:26 -0500

    Card.js

--- 01-starting-project/src/components/users/AddUser.js
+++ 01-starting-project/src/components/users/AddUser.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './AddUser.css';
+import Card from '../ui/Card'
+
+const AddUser = props => {
+    function addUserHandler(event){
+        event.preventDefault();
+    }
+
+    return (
+        <Card>
+        <form onSubmit = {addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text"></input>
+            <label htmlFor="age">Age (years)</label>
+            <input id="age" type="number"></input>
+            <button type="submit">Add User</button>
+  
+        </form></Card>
+    );
+};
+export default AddUser;
