831344b9f3b2b8fd539a39d7c9d9094f948a38be
Abby Winn <abbywinn14@gmail.com>
2022-06-21 11:12:32 -0500

    Added custom error messages

---
+++ 01-starting-project/src/components/ui/ErrorModal.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./ErrorModal.css";
+import Card from "./Card.js";
+import Button from "./Button.js";
+
+const ErrorModal = props => {
+    return (
+        <div>
+            <div className="backdrop" onClick={props.onHandleError}/>
+            <div className="modal">
+                <Card>
+                    <header className="header">
+                        <h2>{props.title}</h2>
+                    </header>
+                    <div className="content">
+                        <p>{props.message}</p>
+                    </div>
+                    <footer className="actions">
+                        <Button clickHandler={props.onHandleError}>Okay</Button>
+                    </footer>
+                </Card>
+            </div>
+        </div>
+    );
+};
+export default ErrorModal; 
