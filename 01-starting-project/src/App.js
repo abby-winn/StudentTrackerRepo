88de9d9a5015ed715fd05d434ec4ed5933d44902
Aman Amjad <aman.amjad@gmail.com>
2022-06-21 12:44:25 -0500

    Delete User

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -5,11 +5,21 @@ import UsersList from "./components/users/UsersList";
 function App() {
   const[usersList, setUsersList] = useState([]);
 
+  function deleteUserHandler(userId){
+    const userIndex = usersList.findIndex(user =>
+      user.id === userId,
+      );
+      const updatedUsers = [...usersList];
+      updatedUsers[userIndex].active = false;
+      setUsersList(updatedUsers);
+  }
+  
   function addUserHandler(username, age) {
     const newUser = {
       name: username,
       age: age,
-      id: Math.random().toString()
+      id: Math.random().toString(),
+      active: true,
     };
     setUsersList((prevUsersList) => {
       return [...prevUsersList, newUser];
@@ -20,7 +30,7 @@ function App() {
   return (
     <div>
       <AddUser onAddUser={addUserHandler}></AddUser>
-      <UsersList users = {usersList}></UsersList>
+      <UsersList users = {usersList} deleteUser = {deleteUserHandler}></UsersList>
     </div>
   );
 }
