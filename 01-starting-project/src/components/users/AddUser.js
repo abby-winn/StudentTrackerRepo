6de5391ff862ba0e8b9ad45d45c366eeee5f2ca6
Abby Winn <abbywinn14@gmail.com>
2022-06-21 10:02:09 -0500

    Added Card.js

--- 01-starting-project/src/components/users/AddUser.js
+++ 01-starting-project/src/components/users/AddUser.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './AddUser.css';
+import Card from "../ui/Card";
 
 const AddUser = props => {
     function addUserHandler(event) {
         event.preventDefault();
     }
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text"></input>
-            <label htmlFor="age">Age (years)</label>
-            <input id="age" type="number"></input>
-            <button type="submit">Add User</button>
-        </form>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"></input>
+                <label htmlFor="age">Age (years)</label>
+                <input id="age" type="number"></input>
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     );
 };
 export default AddUser; 
