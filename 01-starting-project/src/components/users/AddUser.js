c6462bf963f5bef0336db102d054805550319134
Alan Sudhan <23sudhanalan@gmail.com>
2022-06-21 10:31:37 -0500

    Integrated the functions to the form

--- 01-starting-project/src/components/users/AddUser.js
+++ 01-starting-project/src/components/users/AddUser.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './AddUser.css';
 import Card from '../ui/Card';
-import Button from '///ui/Button';
+import Button from '../ui/Button';
 
 const AddUser = props => {
 
@@ -17,15 +17,28 @@ const AddUser = props => {
 
     function addUserHandler(event){
         event.preventDefault();
+
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            return;
+        }
+
+        if (+enteredAge < 1) {
+            return;
+        }
+
+        console.log(enteredUsername, enteredAge)
+
+        setEnteredUsername("");
+        setEnteredAge("");
     }
 
     return (
         <Card>
             <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" onChange={usernameChangeHandler}></input>
+            <input id="username" type="text" value = {enteredUsername} onChange={usernameChangeHandler}></input>
             <label htmlFor="age">Age (years)</label>
-            <input id="age" type="number"onChange={ageChangeHandler}></input>
+            <input id="age" type="number" value = {enteredAge} onChange={ageChangeHandler}></input>
             <Button type="submit">Add User</Button>
         </form>
         </Card>
