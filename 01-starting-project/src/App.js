b2d8319213c4b2324b23e822942a711696df8729
Tommy Terry <tommyterry2005@gmail.com>
2022-06-21 12:44:36 -0500

    delete user

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -5,11 +5,19 @@ import UsersList from './components/user/UsersList';
 function App() {
   const [usersList, setUsersList] = useState([]);
 
+  function deleteUserHandler(userId) {
+    const userIndex = usersList.findIndex(user => user.id === userId);
+    const updatedUsers = [...usersList];
+    updatedUsers[userIndex].active = false;
+    setUsersList(updatedUsers);
+  }
+
   function addUserHandler(username, age) {
     const newUser = {
       name: username,
       age: age,
-      id: Math.random().toString()
+      id: Math.random().toString(),
+      active: true
     };
     setUsersList((prevUsersList) => {
       return [...prevUsersList, newUser];
@@ -20,7 +28,7 @@ function App() {
   return (
     <div>
         <Adduser onAddUser={addUserHandler}></Adduser>
-        <UsersList users={usersList}></UsersList>
+        <UsersList users={usersList} deleteUser={deleteUserHandler}></UsersList>
     </div>
   );
 }
--- 01-starting-project/src/components/user/UsersList.js
+++ 01-starting-project/src/components/user/UsersList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './UsersList.css'
 import Card from '../ui/Card.js';
+import Button from '../ui/Button.js';
 const UsersList = props => {
+    let activeUsers = props.users.filter(user => user.active === true);
+    function deleteHandler(event) {
+        props.deleteUser(event.target.value);
+    }
     return (
         <Card>
             <ul className="users">
-                {props.users.map(user =>
-                    <li key ={user.id}> {user.name} ({user.age} years old)</li>
+                {activeUsers.map(user =>
+                    <li key ={user.id}> {user.name} ({user.age} years old)
+                    <Button clickHandler={deleteHandler} value={user.id}>Delete</Button>
+                    </li>
                 )}
             </ul>
         </Card>
